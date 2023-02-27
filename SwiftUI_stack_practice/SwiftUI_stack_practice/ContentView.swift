593d1a104725ddfecdc6b017a345dedf43834601
leolim <dudwnssss@naver.com>
2023-02-27 09:35:09 +0900

    List는 UIkit의 TableView, 네비게이션바 실습

--- SwiftUI_stack_practice/SwiftUI_stack_practice/ContentView.swift
+++ SwiftUI_stack_practice/SwiftUI_stack_practice/ContentView.swift
@@ -8,56 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isNavigationBarHidden : Bool = false
+
     var body: some View {
-        
-        
-        
-        ZStack(alignment: .bottomTrailing ){
-            VStack(alignment: .leading, spacing: 0 ){
-                
-                HStack{
-                    Image(systemName: "line.horizontal.3")
-                        .font(.largeTitle)
-                    Spacer()
-                    Image(systemName: "person.crop.circle.fill")
-                        .font(.largeTitle)
-                }
-                .padding(  20 )
-       
-                
-                Text("정대리 할 일목록 ")
-                    .font(.system(size:40 ))
-                    .fontWeight(.black)
-                    .padding(.horizontal)
-                 
-                ScrollView{
-                    VStack {
-                        MyProjectCard()
-                        MyBasicCard()
-                        MyCard(icon: "tray.fill", title: " ", start: "10 AM", end: "11 AM", bgColor: Color.blue)
-                        MyCard(icon: "tray.fill", title: "영상 다시보기", start: "10 PM", end: "11 AM", bgColor: Color.orange)
-                        MyCard(icon: "tray.fill", title: "마트 장보기", start: "10 AM",  end: "11 AM", bgColor: Color.green)
-
+        NavigationView{
+            ZStack(alignment: .bottomTrailing ){
+                VStack(alignment: .leading, spacing: 0 ){
+                    
+                    HStack{
+                        
+                        NavigationLink(destination: MyList(isNavigationBarHidden:  self.$isNavigationBarHidden)){
+                            Image(systemName: "line.horizontal.3")
+                                .font(.largeTitle)
+                                .foregroundColor(Color.black)
+                        }
+                        
+                        Spacer()
+                        Image(systemName: "person.crop.circle.fill")
+                            .font(.largeTitle)
                     }
-                    .padding()
+                    .padding(  20 )
+                    
+                    
+                    Text("정대리 할 일목록 ")
+                        .font(.system(size:40 ))
+                        .fontWeight(.black)
+                        .padding(.horizontal)
+                    
+                    ScrollView{
+                        VStack {
+                            MyProjectCard()
+                            MyBasicCard()
+                            MyCard(icon: "tray.fill", title: " ", start: "10 AM", end: "11 AM", bgColor: Color.blue)
+                            MyCard(icon: "tray.fill", title: "영상 다시보기", start: "10 PM", end: "11 AM", bgColor: Color.orange)
+                            MyCard(icon: "tray.fill", title: "마트 장보기", start: "10 AM",  end: "11 AM", bgColor: Color.green)
+                            
+                        }
+                        .padding()
+                    }
                 }
-            }
-
-            
-            Circle()
-                .foregroundColor(Color.yellow)
-                .frame(width: 60, height: 60 )
-                .overlay(
-                    Image(systemName: "plus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                )
-            
-            
-                .padding( 10 )
-                .shadow(radius: 20 )
-        }
-        
+                
+                
+                Circle()
+                    .foregroundColor(Color.yellow)
+                    .frame(width: 60, height: 60 )
+                    .overlay(
+                        Image(systemName: "plus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                    )
+                
+                
+                    .padding( 10 )
+                    .shadow(radius: 20 )
+            }//Zstack
+            .navigationTitle("뒤로가기")
+            .navigationBarHidden(self.isNavigationBarHidden)
+            .onAppear{
+                self.isNavigationBarHidden = true
+            }        }//NavigationView
         
     }
 }
@@ -67,3 +76,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
