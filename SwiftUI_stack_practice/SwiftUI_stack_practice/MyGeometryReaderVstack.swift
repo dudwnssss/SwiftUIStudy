b9dc4b306b2a0f364dd5c442b03c6aaed432318e
leolim <dudwnssss@naver.com>
2023-02-27 21:23:24 +0900

    지오메트리리더 프록시 설정 및 클로져 활용

--- SwiftUI_stack_practice/SwiftUI_stack_practice/MyGeometryReaderVstack.swift
+++ SwiftUI_stack_practice/SwiftUI_stack_practice/MyGeometryReaderVstack.swift
@@ -15,12 +15,16 @@ struct MyGeometryReaderVstack: View {
     
     @State var index : Index = .one
     
+    // 지오메트리 프록시를 매개변수로 가지고 CGPoint를 반환하는 클로져
+    let centerPosition : (GeometryProxy) -> CGPoint = { proxy in
+        return CGPoint(x: proxy.frame(in: .local).midX, y: proxy.frame(in: .local).midY)
+    }
+    
     var body: some View {
         
         
         GeometryReader{
-            
-            geometry in
+            proxy in
             
             VStack(spacing: 0){
                 
@@ -36,7 +40,7 @@ struct MyGeometryReaderVstack: View {
                     Text("1")
                         .font(.largeTitle)
                         .fontWeight(.black)
-                        .frame(width: 100, height: geometry.size.height/3)
+                        .frame(width: 100, height: proxy.size.height/3)
                         .padding(.horizontal, self.index == .one ? 50 : 0)
                         .foregroundColor(Color.white)
                         .background(Color.red)
@@ -50,7 +54,7 @@ struct MyGeometryReaderVstack: View {
                     Text("2")
                         .font(.largeTitle)
                         .fontWeight(.black)
-                        .frame(width: 100, height: geometry.size.height/3)
+                        .frame(width: 100, height: proxy.size.height/3)
                         .padding(.horizontal, self.index == .two ? 50 : 0)
                         .foregroundColor(Color.white)
                         .background(Color.blue)
@@ -66,12 +70,14 @@ struct MyGeometryReaderVstack: View {
                     Text("3")
                         .font(.largeTitle)
                         .fontWeight(.black)
-                        .frame(width: 100, height: geometry.size.height/3)
+                        .frame(width: 100, height: proxy.size.height/3)
                         .padding(.horizontal, self.index == .three ? 50 : 0)
                         .foregroundColor(Color.white)
                     .background(Color.green)
                 }
             }
+//            .position(CGPoint(x: proxy.frame(in: .local).midX, y: proxy.frame(in: .local).midY))
+            .position(centerPosition(proxy))
         }
         .background(Color.yellow)
         .edgesIgnoringSafeArea(.all)
