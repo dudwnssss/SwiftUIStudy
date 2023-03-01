92a8db8c842276147d9d8be5eece7d171a419c41
leolim <dudwnssss@naver.com>
2023-03-01 11:02:35 +0900

    텍스트 입력

---
+++ TextInput_SwiftUI/TextInput_SwiftUI/TextInput_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInput_SwiftUIApp.swift
+//  TextInput_SwiftUI
+//
+//  Created by 임영준 on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct TextInput_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
